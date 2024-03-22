da2422a7d0acb624482fbcf7b140468cfe207f40
Maciej Kowalczyk <mm.kowalczykk@gmail.com>
2024-03-22 13:11:51 +0100

    Added starter class for spring boot application and context test

---
+++ src/test/java/pl/kowalczyk/maciej/spring/multitoolapp/MultitoolAppApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.kowalczyk.maciej.spring.multitoolapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MultitoolAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
